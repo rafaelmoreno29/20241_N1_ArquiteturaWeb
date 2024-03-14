92f9b1d354132487468a7dd6886c4f383a4c724a
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-03-13 22:24:14 -0300

    JPA Relacionamento

---
+++ projetoescola/src/main/java/com/example/projetoescola/controllers/CategoriaCursoController.java
@@ -0,0 +1,36 @@
+package com.example.projetoescola.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.projetoescola.models.CategoriaCurso;
+import com.example.projetoescola.repositories.CategoriaCursoRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("api/categoria-curso")
+public class CategoriaCursoController {
+    private CategoriaCursoRepository categoriaCursoRepository;
+
+    public CategoriaCursoController(
+            CategoriaCursoRepository categoriaCursoRepository) {
+        this.categoriaCursoRepository = categoriaCursoRepository;
+    }
+
+    @GetMapping()
+    public List<CategoriaCurso> obterTodos() {
+        return categoriaCursoRepository.obterTodos();
+    }
+
+    @PostMapping()
+    public void inserir(@RequestBody CategoriaCurso categoria) {
+        categoriaCursoRepository.inserir(categoria);
+    }
+
+}
